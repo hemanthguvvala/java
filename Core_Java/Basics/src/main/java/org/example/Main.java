ab702a5a209f02fac64376dafeb794445f15cb3a
Hemanth Kumar Guvvala <khemanth00321@gmail.com>
2025-04-26 16:09:43 +0530

    Intial Code commits

---
+++ Core_Java/Basics/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //This is the initial state where jvm finds the main function of byteCode
+        /**
+         * and also if you know, how to compile a java class, then only choose this IDE's orelse you can fully
+         * understand the concept.
+         * Anyway, i will explain, when the class file saved, you need to compile it first
+         * with javac fileName.java
+         * and then bytecode will be generated and you need to run it with
+         *  -- java className
+         */
+        System.out.println("Hello world!");
+    }
+}
