a78e1055c53243b0965ed9d7465f8abd6cc5aab9
Hemanth Kumar Guvvala <khemanth00321@gmail.com>
2025-04-30 22:54:18 +0530

    java 8+ features

--- Core_Java/Basics/src/main/java/org/example/Java8Features.java
+++ Core_Java/Basics/src/main/java/org/example/Java8Features.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Java8Features {
     public static void main(String[] args) {
@@ -12,5 +15,17 @@ public class Java8Features {
 
         Optional<String> optionalWithOf = Optional.of(name);
         System.out.println(optionalWithOf);
+
+        List<String> data = null;
+        var steam = Stream.ofNullable(data);
+
+        steam.forEach(System.out::println);
+
+        var data2 = new ArrayList<>();
+        data2.add("abc");
+        data2.add("baca");
+        Stream<Object> steam2 = Stream.ofNullable(data2);
+
+        steam2.forEach(System.out::println);
     }
 }
